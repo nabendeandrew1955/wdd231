ba4a70458bde9f8915ad03d8d11a9fef09814c5c
nabendeandrew1955 <nmafabi@byupathway.edu>
2026-07-02 12:47:30 +0300

    created navigation javascript

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,5 +1,13 @@
-const navbutton = document.querySelector('#ham-btn')
+const navbutton = document.querySelector('#ham-btn');
+const navlinks = document.querySelector('#nav-bar');
+let d = new Date();
+document.getElementById("year").innerHTML = `&copy; ${d.getFullYear()}`;
+document.querySelector("#lastmodified").textContent = `lastmodification: ${document.lastModified}`;
+
+
+
 
 navbutton.addEventListener('click', () => {
     navbutton.classList.toggle('show');
+    navlinks.classList.toggle('show');
 });
