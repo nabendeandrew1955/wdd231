2c4a15766980ecc835311851c6628ffc43f6a195
nabendeandrew1955 <nmafabi@byupathway.edu>
2026-07-21 03:10:59 +0300

    added images

--- chamber/scripts/w2eather.js
+++ chamber/scripts/w2eather.js
@@ -3,13 +3,19 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 const base = document.querySelector('#town'); 
+let d = new Date();
+document.getElementById("year").innerHTML = `&copy; ${d.getFullYear()}`;
+document.querySelector("#lastmodified").textContent = `lastmodification: ${document.lastModified}`;
+
 
 
 const mykey = "045fe0b9399a754d351a447d1a248004"
-const mylat = "49.78"
-const mylog = "6.45"
+const mylat = "1.512485"
+const mylog = "32.395955"
+const mycnt = 3
 
-const myurl = `//api.openweathermap.org/data/2.5/weather?lat=${mylat}&lon=${mylog}&"units=imperial"&appid=${mykey}&units=imperial`;
+ const myurl = `//api.openweathermap.org/data/2.5/weather?lat=${mylat}&lon=${mylog}&"units=imperial"&appid=${mykey}&units=imperial`;
+//const myurl = `//api.openweathermap.org/data/2.5/forecast/daily?lat=${mylat}&lon=${mylog}&cnt=${mycnt}&appid=${mykey}`;
 
 async function apiFetch() {
     try {
@@ -32,7 +38,7 @@ function displayResults(data) {
     currentTemp.innerHTML = `${data.main.temp}&deg;F`;
     base.innerHTML = data.sys.country;
 
-    const iconsrc = `//openweathermap.org/payload/api/media/file/${data.weather[0].icon}%402x.png`;
+    const iconsrc = `https://openweathermap.org/payload/api/media/file/${data.weather[0].icon}%402x.png`;
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', data.weather[0].description);
 
