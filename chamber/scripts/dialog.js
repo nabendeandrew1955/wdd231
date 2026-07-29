fbd06b810187809a1a587dc6f3d6f23ad3b11b35
nabendeandrew1955 <nmafabi@byupathway.edu>
2026-07-30 00:49:17 +0300

    modified the dialog

--- chamber/scripts/dialog.js
+++ chamber/scripts/dialog.js
@@ -7,48 +7,49 @@ const dialogBox = document.querySelector("#mydialog");
 const dialogBoxText = document.querySelector("#mydialog div");
 const myTitle = document.querySelector("#mydialog h2");
 const myInfo = document.querySelector("#mydialog p");
+const myInfo2 = document.querySelector("#mydialog p");
 const closeButton = document.querySelector("#closeButton");
 
 openButton.addEventListener("click", () => {
     myTitle.innerHTML = "Np Membership";
-    myInfo.innerHTML = "Bespoke package tailored to your strategic goals.";
-    myInfo.innerHTML = "Unrivalled access to leadership forums and policy - shaping opportunities.";
-    myInfo.innerHTML = "Premium brand exposure across all Chamber platforms and events.";
-    myInfo.innerHTML = "Thought leadership opportunities to elevate your brand.";
-    myInfo.innerHTML = "Dedicated support for your leadership and team development.";
-    myInfo.innerHTML = "Be a voice for the region — help shape its future.";
+    myInfo.innerHTML = `<p>✔️Bespoke package tailored to your strategic goals.</p>
+     <p>✔️Unrivalled access to leadership forums and policy - shaping opportunities</p>
+    <p>✔️Premium brand exposure across all Chamber platforms and events.</p>
+    <p>✔️Thought leadership opportunities to elevate your brand.</p>
+    <p>✔️Dedicated support for your leadership and team development</p>
+    <p>✔️Be a voice for the region — help shape its future.</p>`
 
     dialogBox.showModal();
 });
 
 openButton2.addEventListener("click", () => {
     myTitle.innerHTML = "Gold Membership";
-    myInfo.innerHTML = "Enhanced exposure on Chamber platforms and website priority listing";
-    myInfo.innerHTML = "VIP invitations to sector - specific and strategic events(e.g., MP Forums, Bank of England)";
-    myInfo.innerHTML = "Membership certificate and promotional photo opportunities.";
-    myInfo.innerHTML = "Access to the prestigious Black Country Business Club(T & C's apply).";
-    myInfo.innerHTML = "6 - monthly in -person account reviews with your Business Relationship Manager";
+    myInfo.innerHTML = `<p>✔️Enhanced exposure on Chamber platforms and website priority listing</p>
+    <p>✔️VIP invitations to sector - specific and strategic events(e.g., MP Forums, Bank of England)</p>
+    <p>✔️Membership certificate and promotional photo opportunities.</p>
+    <p>✔️Access to the prestigious Black Country Business Club(T & C's apply).</p>
+    <p>✔️ "6 - monthly in -person account reviews with your Business Relationship Manager</p>`
     dialogBox.showModal();
 });
 
  openButton3.addEventListener("click", () => {
      myTitle.innerHTML = "Silver Membership";
-     myInfo.innerHTML = "Exclusive access to the Platinum Group – senior - level peer network.";
-     myInfo.innerHTML = "Bespoke business review identifying growth opportunities in the region.";
-     myInfo.innerHTML = "Invitations to high-impact policy and economic briefings (i.e. Bank of England, MP Forums)";
-     myInfo.innerHTML = "Shape conversations around regional growth and business policy.";
-     myInfo.innerHTML = "Share insights across sectors and gain fresh strategic perspectives.";
-     myInfo.innerHTML = "Share best practice, knowledge, information and expertise to build strong and trusted business relationships.";
+     myInfo.innerHTML = `<p>✔️Exclusive access to the Platinum Group – senior - level peer network.</p>
+     <p>✔️Bespoke business review identifying growth opportunities in the region.</p>
+     <p>✔️Invitations to high-impact policy and economic briefings (i.e. Bank of England, MP Forums)</p>
+     <p>✔️Shape conversations around regional growth and business policy.</p>
+     <p>✔️Share insights across sectors and gain fresh strategic perspectives.</p>
+     <p>✔️Share best practice, knowledge, information and expertise to build strong and trusted business relationships.</p>`
     dialogBox.showModal();
  });
  openButton4.addEventListener("click", () => {
      myTitle.innerHTML = "Bronze Membership"
-     myInfo.innerHTML = "Bespoke package tailored to your strategic goals."
-     myInfo.innerHTML = "Unrivalled access to leadership forums and policy-shaping opportunities."
-     myInfo.innerHTML = "Premium brand exposure across all Chamber platforms and events."
-     myInfo.innerHTML = "Thought leadership opportunities to elevate your brand."
-     myInfo.innerHTML = "Dedicated support for your leadership and team development."
-     myInfo.innerHTML = "Be a voice for the region — help shape its future."
+     myInfo.innerHTML = `<p>✔️Bespoke package tailored to your strategic goals.</p>
+     <p>✔️Unrivalled access to leadership forums and policy-shaping opportunities.</p>
+     <p>✔️Premium brand exposure across all Chamber platforms and events.</p>
+     <p>✔️Thought leadership opportunities to elevate your brand.</p>
+     <p>✔️Dedicated support for your leadership and team development.</p>
+     <p>✔️Be a voice for the region — help shape its future.</p>`
      dialogBox.showModal();
  });
 
