8cc45ac5bda09f499cfc402b16816b3dc563a5d6
nabendeandrew1955 <nmafabi@byupathway.edu>
2026-08-04 11:07:32 +0300

    created the discover page

---
+++ chamber/data/discover.mjs
@@ -0,0 +1,54 @@
+
+
+const company = [
+    {
+        "Name": "vibrant-tourism",
+        "Address": "Bwindi National Park",
+        "Description": "Uganda is a top choice for tourists seeking authentic experiences in Africa. Mountain Gorillas are found only in three countries, Uganda, Congo and Rwanda but Uganda has more than half the whole population.",
+        "path": "images/gorilla-trekking-in-uganda.webp"   
+    },
+    {
+        "Name": "Cultural Tourism",
+        "Address": "Kasubi Tombs",
+        "Description": "Uganda has major ethic groups and tribes. all with their cultural practices and norms",
+        "path": ""
+    },
+    {
+        "Name": "Renewable Energy",
+        "Address": "Kampala",
+        "Description": "Government incentives for this renewable energy.Energy projects further enhance the appeal of this sector",
+        "path": "images/renewable-energy-solutions.webp"
+    },
+    {
+        "Name": "Vaccine Production",
+        "Address": "Lugard Avenue Entebbe",
+        "Description": "It holds its national mandate for spearheading initiatives that enhance agricultural productivity and ensure food security.",
+        "path": "images/agricultural-research.webp"
+    },
+    {
+        "Name": "Maize Garden",
+        "Address": "Mbale",
+        "Description": "Small holder farmers low productivity means they hardly meet their household food needs and are unable to participate in the market",
+        "path": "images/maize-garden.webp"
+    },
+    {
+        "Name": "Agricultural Land",
+        "Address": "Old Kira Road",
+        "Description": "The Agricultural Sector continues to be the most important sector in Uganda. It employs approximately 69% of the population and contributes about 26% of the population",
+        "path": "images/agriculture-land-in-uganda.webp"
+    },
+    {
+        "Name": "More-productivity-farming",
+        "Address": "Jinja",
+        "Description": "With Agriculture employing 70% of Ugandans, there is need to close potential performance divide through comercialisation, value-addition and trade, according to a new report",
+        "path": "images/more-productive-farming.webp"
+    },
+    {
+        "Name": "Cultivating-equity",
+        "Address": "Mbarara",
+        "Description": "Women produce most of the Food we enjoy at Home. Efforts are needed to up their productivity to match male counter parts",
+        "path": "images/cultivating-equity.webp"
+    }
+
+
+]
